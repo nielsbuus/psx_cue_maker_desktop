e3c59a0b253c7adb8de1abe8984539bbaea3ac20
Claus Jensby Madsen <clausjensbymadsen@gmail.com>
2017-04-29 13:14:10 +0200

    Add directory selection dialog box

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,67 @@
 #include <vector>
 #include <iostream>
 
+#include "Objbase.h"
+#include "Shlobj.h"
+
 using namespace std;
 
-int main(int argc, const char* argv[]) {
-  if (argc > 1) {
-    cout << "FILE \"" << argv[0] << "\" BINARY\n";
+TCHAR select_directory_path[MAX_PATH];
+
+class COM {
+public:
+  COM() {
+  if (CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED) != S_OK)
+    throw runtime_error("Failed to initialize COM.");
+  }
+  ~COM() {
+    CoUninitialize();
+  }
+};
+
+// Opens directory selection dialog box.
+// Returns true when a directory is selected, returns false when cancelled.
+bool select_directory() {
+  BROWSEINFO browseinfo {};
+  browseinfo.pszDisplayName = select_directory_path;
+  browseinfo.lpszTitle = "Please select directory containing the bin files.";
+  browseinfo.ulFlags = BIF_RETURNONLYFSDIRS | BIF_NEWDIALOGSTYLE | BIF_NONEWFOLDERBUTTON;
+  PIDLIST_ABSOLUTE idlist = SHBrowseForFolder(&browseinfo);
+  if (idlist == nullptr) {
+    return false;
+  }
+  else {
+    if (!SHGetPathFromIDList(idlist, select_directory_path)) {
+      CoTaskMemFree(idlist);
+      throw runtime_error("SHGetPathFromIDList failed.");
+    };
+    CoTaskMemFree(idlist);
+    return true;
+  }
+}
+
+// TODO: A lot of error detection and handling is missing.
+
+int main(int argc, const char* argv[]) {  
+  COM com;  
+  if (select_directory()) {
+    string search_path(select_directory_path);
+    search_path += "\\*.bin";
+
+    WIN32_FIND_DATA search_data {};
+    int track_count = 0;
+    HANDLE search_handle = FindFirstFile(search_path.c_str(), &search_data);
+    cout << "FILE \"" << search_data.cFileName << "\" BINARY\n";
     cout << "TRACK 01 MODE2/2352\n";
     cout << "INDEX 01 00:00:00\n";
-  
-    for(int track = 1; track < argc; ++track) {
-      cout << "FILE \"" << argv[track] << "\" BINARY\n";
-      cout << "TRACK 0" << track << " AUDIO\n";
+    while (FindNextFile(search_handle, &search_data)) {
+      ++track_count;
+      cout << "FILE \"" << search_data.cFileName << "\" BINARY\n";
+      cout << "TRACK 0" << track_count << " AUDIO\n";
       cout << "INDEX 00 00:00:00\n";
       cout << "INDEX 01 00:02:00\n";
     }
-  }
-  else {
-    cerr << "Usage: PSXCueMaker binfile1 [binfile2] [...] > cuefile.cue\n";
+    FindClose(search_handle);
   }
   
   return 0;
