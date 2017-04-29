f17876248d61e52386d12717cfbb9b82f633123a
CM <CM>
2017-04-29 17:09:43 +0200

    Refactor main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <vector>
-#include <iostream>
 #include <sstream>
+#include <experimental/optional>
 
 #include "windows.h"
 #include "Objbase.h"
 #include "Shlobj.h"
 
 using namespace std;
+using namespace std::experimental;
 
 TCHAR select_directory_path[MAX_PATH];
+
+// TODO: A lot of error detection and handling is missing.
 
 class COM {
 public:
@@ -21,16 +24,14 @@ public:
   }
 };
 
-// Opens directory selection dialog box.
-// Returns true when a directory is selected, returns false when cancelled.
-bool select_directory() {
+optional<string> select_directory() {
   BROWSEINFO browseinfo {};
   browseinfo.pszDisplayName = select_directory_path;
   browseinfo.lpszTitle = "Please select directory containing the bin files.";
   browseinfo.ulFlags = BIF_RETURNONLYFSDIRS | BIF_NEWDIALOGSTYLE | BIF_NONEWFOLDERBUTTON;
   PIDLIST_ABSOLUTE idlist = SHBrowseForFolder(&browseinfo);
   if (idlist == nullptr) {
-    return false;
+    return {};
   }
   else {
     if (!SHGetPathFromIDList(idlist, select_directory_path)) {
@@ -38,37 +39,55 @@ bool select_directory() {
       throw runtime_error("SHGetPathFromIDList failed.");
     };
     CoTaskMemFree(idlist);
-    return true;
+    return select_directory_path;
   }
 }
 
-// TODO: A lot of error detection and handling is missing.
+
+vector<string> find_bin_files(string directory) {
+  vector<string> result;
+  
+  string search_path(directory);
+  search_path += "\\*.bin";
+
+  WIN32_FIND_DATA search_data {};
+  HANDLE search_handle = FindFirstFile(search_path.c_str(), &search_data);
+  result.emplace_back(search_data.cFileName);
+  while (FindNextFile(search_handle, &search_data)) {
+    result.emplace_back(search_data.cFileName);
+  }
+  FindClose(search_handle);
+  
+  return result;
+}
+
+string generate_cuesheet(vector<string> files) {
+  stringstream ss;
+
+  if (files.size() > 0) {
+    ss << "FILE \"" << files.at(0) << "\" BINARY\n";
+    ss << "  TRACK 01 MODE2/2352\n";
+    ss << "    INDEX 01 00:00:00\n";
+    for(size_t track = 1; track < files.size(); ++track) {
+      ss << "FILE \"" << files.at(track) << "\" BINARY\n";
+      ss << "  TRACK 0" << track << " AUDIO\n";
+      ss << "   INDEX 00 00:00:00\n";
+      ss << "   INDEX 01 00:02:00\n";
+    };
+  }
+  
+  return ss.str();
+}
 
 int main(int argc, const char* argv[]) {  
   COM com;  
-  if (select_directory()) {
-    string search_path(select_directory_path);
-    search_path += "\\*.bin";
-
-    WIN32_FIND_DATA search_data {};
-    int track_count = 0;
-    HANDLE search_handle = FindFirstFile(search_path.c_str(), &search_data);
-	
-	stringstream ss;
-	
-    ss << "FILE \"" << search_data.cFileName << "\" BINARY\n";
-    ss << "  TRACK 01 MODE2/2352\n";
-    ss << "    INDEX 01 00:00:00\n";
-    while (FindNextFile(search_handle, &search_data)) {
-      ++track_count;
-      ss << "FILE \"" << search_data.cFileName << "\" BINARY\n";
-      ss << "  TRACK 0" << track_count << " AUDIO\n";
-      ss << "   INDEX 00 00:00:00\n";
-      ss << "   INDEX 01 00:02:00\n";
-    }
-    FindClose(search_handle);
-	
-	MessageBox(nullptr, ss.str().c_str(), "Cuesheet", MB_OK | MB_ICONINFORMATION);
+  
+  auto dir = select_directory();
+  if (dir) {
+    auto files = find_bin_files(*dir);
+    auto cuesheet = generate_cuesheet(files);
+  
+    MessageBox(nullptr, cuesheet.c_str(), "Cuesheet", MB_OK | MB_ICONINFORMATION);
   }
   
   return 0;
